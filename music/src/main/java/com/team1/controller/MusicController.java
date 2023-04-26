c0d237f3e4905af248c36b6cb29331e5fe8f5e02
blinKiss <blinkiss@naver.com>
2023-04-26 15:44:32 +0900

    26

---
+++ music/src/main/java/com/team1/controller/MusicController.java
@@ -0,0 +1,41 @@
+package com.team1.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.team1.domain.Music;
+import com.team1.mapper.MusicMapper;
+import com.team1.service.MusicService;
+
+@Controller
+public class MusicController {
+
+	@Autowired
+	private MusicService service;
+	
+	@Autowired
+	private MusicMapper mapper;
+	
+	@GetMapping("/music/music")
+	public String music(Model model) throws Exception {
+		
+		List<Music> musicList = service.list();
+		
+		model.addAttribute("musicList", musicList);
+		
+		return "/music/music";
+	}
+	
+	@GetMapping("/music/genre/{genre}")
+	public String getMusicByGenre(@PathVariable("genre") String genre, Model model) {
+	    // 매퍼를 호출하여 genre에 해당하는 데이터를 추출
+	    List<Music> musicList = mapper.selectMusicByGenre(genre);
+	    model.addAttribute("musiclist", musicList);
+	    return "music";
+	}
+}
